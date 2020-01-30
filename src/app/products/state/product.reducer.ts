3493ec32ff57749859c62770e89f43f5d9747ef6
wangmta <wangmta@gmail.com>
2020-01-30 14:14:35 -0500

    feat: build selector inside reducer

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -1,5 +1,6 @@
 import { Product } from '../product';
 import * as FromRoot from '../../app.state';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
 
 // this represents the entire state tree
 export interface State extends FromRoot.State {
@@ -18,6 +19,25 @@ const initialState: ProductState = {
   currentProduct: null,
   products: []
 };
+
+// selector state, order matters
+const getProductFeatureState = createFeatureSelector<ProductState>('');
+
+// selector
+export const getShowProductCode = createSelector(
+  getProductFeatureState,
+  state => state.showProductCode // projector function, returns only the desired data
+);
+
+export const getCurrentProduct = createSelector(
+  getProductFeatureState,
+  state => state.currentProduct
+);
+
+export const getProducts = createSelector(
+  getProductFeatureState,
+  state => state.products
+);
 
 // reducer takes in the original state, the action dispatched by the component, and last return the new state
 export function reducer(state = initialState, action): ProductState {
